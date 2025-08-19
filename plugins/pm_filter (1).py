5939efef685d0884b9aefe355ba7317d35667999
Muhammed Aflah <aflahtbm@gmail.com>
2025-08-19 07:31:53 +0530

    Update pm_filter (1).py

--- plugins/pm_filter (1).py
+++ plugins/pm_filter (1).py
@@ -1,4 +1,4 @@
-# Kanged From @TroJanZheX
+
 import asyncio,re,ast,math,logging,tgcrypto,time,shutil,psutil,os,sys
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
@@ -720,7 +720,7 @@ async def auto_filter(client, msg):
             [InlineKeyboardButton(text="📃 1/1", callback_data="pages")]
         )
 
-    cap=f"<b>𝖧𝖾𝗒{message.from_user.mention}\n👇𝙏𝙖𝙥 𝙤𝙣 𝙩𝙝𝙚 𝙛𝙞𝙡𝙚 𝙗𝙪𝙩𝙩𝙤𝙣 𝙖𝙣𝙙 𝙩𝙝𝙚𝙣 𝙨𝙩𝙖𝙧𝙩 𝙩𝙤 𝙙𝙤𝙬𝙣𝙡𝙤𝙖𝙙.👇</b>"  # result for group
+    cap=f"Search Query:<a>{title}<b>𝖧𝖾𝗒{message.from_user.mention}\n👇𝙏𝙖𝙥 𝙤𝙣 𝙩𝙝𝙚 𝙛𝙞𝙡𝙚 𝙗𝙪𝙩𝙩𝙤𝙣 𝙖𝙣𝙙 𝙩𝙝𝙚𝙣 𝙨𝙩𝙖𝙧𝙩 𝙩𝙤 𝙙𝙤𝙬𝙣𝙡𝙤𝙖𝙙.👇</b>"  # result for group
 
     # Send the message with the search results and buttons
     autodel = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
